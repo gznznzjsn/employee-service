e6c5e542e5edeb000f215024b74865b43622ffcf
gznznzjsn <arik2046@gmail.com>
2023-03-09 18:39:18 +0300

    Refactored

--- src/main/java/com/gznznzjsn/employeeservice/exception/IllegalActionException.java
+++
@@ -1,9 +0,0 @@
-package com.gznznzjsn.employeeservice.exception;
-
-public class IllegalActionException extends RuntimeException {
-
-    public IllegalActionException(String message) {
-        super(message);
-    }
-
-}
--- src/main/java/com/gznznzjsn/employeeservice/exception/UniqueResourceException.java
+++
@@ -1,9 +0,0 @@
-package com.gznznzjsn.employeeservice.exception;
-
-public class UniqueResourceException extends RuntimeException {
-
-    public UniqueResourceException(String message) {
-        super(message);
-    }
-
-}
